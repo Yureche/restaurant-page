2d25e257440ca8ae5ac83652901f231a4cb6895a
Yuri <yurigerber.g18@gmail.com>
2021-12-02 13:17:04 +0200

    Home changed to about

--- src/header.js
+++ src/header.js
@@ -1,7 +1,6 @@
 export default function showHeader() {
-  const CONTENT = document.getElementById("content");
   const HEADER = document.createElement("header");
-  CONTENT.appendChild(HEADER);
+  document.body.appendChild(HEADER);
 
   const LOGO = document.createElement("h1");
   LOGO.textContent = "Restaurant";
@@ -11,14 +10,15 @@ export default function showHeader() {
   const NAVBAR = document.createElement("div");
   NAVBAR.classList.add("navbar");
   HEADER.appendChild(NAVBAR);
-  const HOME = document.createElement("p"),
-    MENU = document.createElement("p"),
-    ORDER = document.createElement("p");
-  HOME.textContent = "Home";
-  HOME.classList.add("navbar_item");
+  const MENU = document.createElement("p"),
+    ORDER = document.createElement("p"),
+    ABOUT = document.createElement("p");
+
   MENU.textContent = "Menu";
   MENU.classList.add("navbar_item");
   ORDER.textContent = "Order";
   ORDER.classList.add("navbar_item");
-  NAVBAR.append(HOME, MENU, ORDER);
+  ABOUT.textContent = "About";
+  ABOUT.classList.add("navbar_item");
+  NAVBAR.append(MENU, ORDER, ABOUT);
 }
