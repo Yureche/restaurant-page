b1afc760bb7873f35c7ed5464465df5beb5319bd
Yuri <yurigerber.g18@gmail.com>
2021-12-07 22:48:48 +0200

    Small changes

--- src/header.js
+++ src/header.js
@@ -1,29 +1,22 @@
-import { showMenu } from "./menu";
+const HEADER = document.createElement("header");
+document.body.appendChild(HEADER);
 
-export function showHeader() {
-  const HEADER = document.createElement("header");
-  document.body.appendChild(HEADER);
+const LOGO = document.createElement("h1");
+LOGO.textContent = "Restaurant";
+LOGO.classList.add("logo");
+HEADER.appendChild(LOGO);
 
-  const LOGO = document.createElement("h1");
-  LOGO.textContent = "Restaurant";
-  LOGO.classList.add("logo");
-  HEADER.appendChild(LOGO);
+const NAVBAR = document.createElement("div");
+NAVBAR.classList.add("navbar");
+HEADER.appendChild(NAVBAR);
+const MENU = document.createElement("p"),
+  ORDER = document.createElement("p"),
+  ABOUT = document.createElement("p");
 
-  const NAVBAR = document.createElement("div");
-  NAVBAR.classList.add("navbar");
-  HEADER.appendChild(NAVBAR);
-  const MENU = document.createElement("p"),
-    ORDER = document.createElement("p"),
-    ABOUT = document.createElement("p");
-
-  MENU.textContent = "Menu";
-  MENU.classList.add("navbar_item");
-  ORDER.textContent = "Order";
-  ORDER.classList.add("navbar_item");
-  ABOUT.textContent = "About";
-  ABOUT.classList.add("navbar_item");
-  NAVBAR.append(MENU, ORDER, ABOUT);
-  MENU.addEventListener("click", () => {
-    showMenu();
-  });
-}
+MENU.textContent = "Menu";
+MENU.classList.add("navbar_item", "navbar_menu");
+ORDER.textContent = "Order";
+ORDER.classList.add("navbar_item", "navbar_order");
+ABOUT.textContent = "About";
+ABOUT.classList.add("navbar_item", "navbar_about");
+NAVBAR.append(MENU, ORDER, ABOUT);
