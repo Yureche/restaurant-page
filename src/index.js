8491ca19e97c038e8a54697cf9c53e30b2a9c3cb
Yuri <yurigerber.g18@gmail.com>
2021-12-08 18:32:11 +0200

    Tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import "./header.js";
 import "./menu.js";
+import "./home.js";
 import "./style.css";
 
-// const MENU_NAVBAR = document.querySelector(".menu_navbar");
 const MENU_NAVBAR = document.querySelector(".navbar_menu");
 const MENU = document.querySelector("select");
 
 MENU_NAVBAR.addEventListener("click", () => {
   MENU.style.display = "block";
+  HOME.style.display = "none";
+});
+
+const HOME_NAVBAR = document.querySelector(".navbar_home");
+const HOME = document.querySelector(".home");
+HOME_NAVBAR.addEventListener("click", () => {
+  HOME.style.display = "flex";
+  MENU.style.display = "none";
 });
